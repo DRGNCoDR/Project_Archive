ccb627217000cba6da013c979b123ec402d350f0
Ray Fradella <rayfradella@yahoo.com>
2024-04-23 05:57:54 -0400

    Update Staff.java

--- Java/Java 102/Firm (Office)/Staff.java
+++ Java/Java 102/Firm (Office)/Staff.java
@@ -16,9 +16,9 @@ public class Staff
      staffList[0]= new Executive("Tony","123 Main Line", "555-0469", "123-45-6789",2423.07,21);
      
      staffList[1]= new Emplyee("Paulie","465 Off Lane","555-0101","987-65-4321",1246.15,14);
-     staffList[2]= new Emplyee("Vito","789 Off Rocker","555-0000","010-20-3040",1169.23,14);
+     staffList[2]= new Emplyee("Vito","789 Off Rocker","555-0000","000-00-0000",1169.23,14);
      
-     staffList[3]= new Hourly ("Michael","678 Fifth Ave.","555-0690","958-47-3625",10.55,7);
+     staffList[3]= new Hourly ("Michael","678 Fifth Ave.","555-0690","000-00-0001",10.55,7);
      
      staffList[4]= new Volunteer("adrianna","987 Babe Blvd.","555-8374");
      staffList[5]= new Volunteer("Benny", "321 Dud Lane","555-7282");
